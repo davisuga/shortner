9d2830101779fa19ef93c0e450f73b8b2cf92965
davisuga <daviciencia1@gmail.com>
2024-01-12 19:29:17 -0300

    Add dynamic URL generation and implement click count increment

--- tests/endpoints.spec.ts
+++ tests/endpoints.spec.ts
@@ -1,8 +1,12 @@
 import { describe, it, expect } from "vitest";
-const exampleUrl = "https://www.google.com";
+const randomNumber = Math.floor(Math.random() * 1000000);
+const exampleUrl = `https://www.google.com?q=${randomNumber}`;
+
+const getTop = async () => (await fetch(`http://localhost:3000/top`)).json();
 
 describe("Shorten URL scrapper", () => {
   let slug = "";
+
   it("should shorten an url", () => {
     fetch("http://localhost:3000/shorten", {
       method: "POST",
@@ -15,9 +19,26 @@ describe("Shorten URL scrapper", () => {
     })
       .then((res) => res.json())
       .then((json) => {
+        console.log("shorten:", json);
         expect(json.shorten).toBeDefined();
+        slug = json.shorten;
       });
   });
 
-  it("should redirect to the original url", () => {});
+  it("should redirect to the original url", () => {
+    fetch(`http://localhost:3000/${slug}`).then((res) => res.status === 302);
+  });
+
+  it("should increment the click count", async () => {
+    console.log(slug);
+    const topResultsBefore = await getTop();
+    console.log(topResultsBefore);
+    await fetch(`http://localhost:3000/${slug}`);
+    const topResultsAfter = await getTop();
+    expect(
+      topResultsAfter.find((result) => result.slug === slug).clicks
+    ).toBeGreaterThan(
+      topResultsBefore.find((result) => result.slug === slug).clicks
+    );
+  });
 });
